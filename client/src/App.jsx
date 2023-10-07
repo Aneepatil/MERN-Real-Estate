8a6fa4d42a74f559b3674993e759f41be93aab2f
Aneepatil <anilspatil8@gmail.com>
2023-10-07 16:40:28 +0530

    changes made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import {Route,Routes} from 'react-router-dom'
+import Header from "./Pages/Header/Header";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Profile from "./Pages/Profile/Profile";
+import SignUp from "./Pages/SignUp/SignUp";
+import SignIn from "./Pages/SignIn/SignIn";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-500">Hello world!</h1>
-    </div>
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+      </Routes>
+    </>
   );
 };
 

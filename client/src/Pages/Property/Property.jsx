489c7527deb037a12a1b1cb04acd8d54dffa7df8
Aneepatil <anilspatil8@gmail.com>
2023-10-15 01:09:11 +0530

    image functionality at ui side

--- client/src/Pages/Property/Property.jsx
+++ client/src/Pages/Property/Property.jsx
@@ -1,6 +1,68 @@
-import React from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import React, { useState } from "react";
+import { app } from "../../firebase";
 
 const Property = () => {
+  const maxFileSize = 2 * 1024 * 1024;
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+        })
+        .catch((error) => {
+          setImageUploadError("Image upload failed 2MB max per image...");
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per property...");
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Progress ${progress}`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-center text-3xl font-semibold my-7 uppercase">
@@ -117,16 +179,36 @@ const Property = () => {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border-green-300 border w-full rounded"
               type="file"
               multiple
               accept="/*"
               id="images"
             />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+            <button
+              type="button"
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+              onClick={handleImageSubmit}
+            >
               upload
             </button>
           </div>
+          {imageUploadError && imageUploadError}
+          {formData.imageUrls.length > 0
+            ? formData.imageUrls.map((url) => (
+                <div className="flex justify-between px-5 border items-center">
+                  <img
+                    src={url}
+                    alt="listing image"
+                    className="w-20 h-20 object-contain rounded-lg"
+                  />
+                  <button className="p-3 text-red-700 rounded-lg hover:opacity-75 uppercase">
+                    Delete
+                  </button>
+                </div>
+              ))
+            : ""}
           <button className="rounded-lg uppercase text-white bg-slate-700 p-3 hover:opacity-75 disabled:opacity-0">
             create property
           </button>
